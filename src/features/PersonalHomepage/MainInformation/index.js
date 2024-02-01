9579b7b0929a636be95680f5faa7ca939d102156
Dorota Borowska <youcode2021db@gmail.com>
2024-02-01 23:57:26 +0100

    react component refactored to tailwindcss, index.css imported

--- src/features/PersonalHomepage/MainInformation/index.js
+++ src/features/PersonalHomepage/MainInformation/index.js
@@ -4,31 +4,45 @@ import { email } from "../../../common/myEmail";
 import envelope from "./envelope.svg";
 import imgProfile from "./imgProfile.jpg";
 import { ThemeSwitcher } from "../ThemeSwitcher";
+import '../../../index.css';
 
 export const MainInformation = () => (
-    <Wrapper>
-        <Avatar src={imgProfile} alt="Dorota Borowska" />
+    <header className="mt-[-30px] grid mid:grid-cols-[auto_1fr] grid-cols-1 mid:gap-16 gap-3 items-center  ">
+        <img
+            className="max-w-[398px] mid:w-30vw w-32 rounded-full mid:mb-16 mb-0"
+            src={imgProfile}
+            alt="Dorota Borowska"
+        />
         <div>
-            <ThisIs>This is</ThisIs>
-            <Name>Dorota Borowska</Name>
-            <Paragraph>Hi there! I am a beginning Frontend Developer with a strong focus on React.
-                I&nbsp;thrive on new challenges in the workplace and enjoy discovering new
-                tools and solutions to improve my&nbsp;work. As a team player, I value building strong
-                relationships with my&nbsp;colleagues and believe in the&nbsp;importance of mutual support,
-                sensitivity, and empathy.
-                Outside of work, I&nbsp;enjoy staying active through sports, as well as taking time to relax and enjoy nature.</Paragraph>
-            <StyledButton
+            <div className=" uppercase font-bold text-xs text-secondaryTextColor ">
+                This is
+            </div>
+            <h1 className="font-black mid:text-4xl text-2xl mid:mt-3 mt-8 text-headerTextColor">
+                Dorota Borowska
+            </h1>
+
+            <p className=" md:text-xl text-base  mx-0 mid:my-8 my-4 leading-snug mid:max-w-2xl max-w-xl text-secondaryTextColor">
+                Hi there! I am a beginning Frontend Developer with a strong focus on
+                React. I thrive on new challenges in the workplace and enjoy discovering
+                new tools and solutions to improve my work. As a team player, I value
+                building strong relationships with my colleagues and believe in the
+                importance of mutual support, sensitivity, and empathy. Outside of
+                work, I enjoy staying active through sports, as well as taking time to
+                relax and enjoy nature.
+            </p>
+            <a
                 href={`mailto:${email}`}
                 title={email}
                 target="_blank"
-                rel="noopener norefereer"
+                rel="noopener noreferrer"
+                className="inline-flex items-center justify-center bg-blueElementColor text-white px-4 py-3 rounded font-semibold text-xl "
             >
-                <Icon src={envelope} />
+                <img src={envelope} alt="Email" className="p-2.5" />
                 Hire Me
-            </StyledButton>
+            </a>
         </div>
 
 
-    </Wrapper>
+    </header>
 
 );
