c4c4e887895f7a125fb50afb23dc42ef71c1c642
Dorota Borowska <youcode2021db@gmail.com>
2024-02-21 21:22:10 +0100

    few dark colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,20 +12,21 @@ module.exports = {
       screens: {
         'mid': '768px',
       },
-      // gridTemplateColumns: {
-      //   'my-columns': 'auto 1fr'
-      // },
       colors: {
         black: "#000",
         headerTextColor: "#252525",
         secondaryTextColor: "#6E7E91",
+        darkSecondaryTextColor: "#fff",
         white: "#fff",
         sectionBackgroundColor: "#fff",
         generalBackground: "#FBFBFE",
         switcherBackground: "#E5E5E5",
+        darkSwitcherBackground: "rgba(54, 54, 54, 0.72)",
         switcherBorder: "#6E7E91",
+        darkSwitcherBorder: "#FFFF",
         blueElementColor: "#0366D6",
         sunColor: "#FFFF",
+        darkSunColor: "#000",
         talesBorder: "rgba(209, 213,218, 0.3)",
         talesBorderHover: "rgba(9,10,51, 0.3)"
       },
