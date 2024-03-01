4abaae1ca2a3a2b72810ef4295cf96b4fb6176ca
Dorota Borowska <youcode2021db@gmail.com>
2024-03-01 13:19:42 +0100

    selectIsDark used

--- src/features/PersonalHomepage/index.js
+++ src/features/PersonalHomepage/index.js
@@ -1,17 +1,23 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import {selectIsDark}from "../PersonalHomepage/ThemeSwitcher/themeSwitcherSlice"
 import { MainInformation } from "./MainInformation";
 import { Skills } from "./Skills";
 import { Portfolio } from "./Portfolio";
-import {Footer}from "./Footer";
+import { Footer } from "./Footer";
 import { ThemeSwitcher } from "./ThemeSwitcher";
 
-export const PersonalHomepage = () => (
-    <div className="container mx-auto mid:p-4 p-3.5 mid:mt-5 mt-2.5">
-        <ThemeSwitcher />
-        <MainInformation />
-        <Skills />
-        <Portfolio />
-        <Footer/>
-    </div>
+export const PersonalHomepage = () => {
+    const isDark = useSelector(selectIsDark);
+    return (
+        <div className={`bg-${isDark ? "darkGeneralBackground" : "generalBackground"} container mx-auto mid:p-4 p-3.5 mid:mt-5 mt-2.5`}>
+            <ThemeSwitcher />
+            <MainInformation />
+            <Skills />
+            <Portfolio />
+            <Footer />
 
-);
+           
+        </div>
+    );
+};
