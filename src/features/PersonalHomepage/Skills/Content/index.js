815cc48a62295010aafc6abb820b8527d12c2bd4
Dorota Borowska <youcode2021db@gmail.com>
2024-03-01 22:55:34 +0100

    in <section> margin bottom increased, in <li> align decreased in mobile

--- src/features/PersonalHomepage/Skills/Content/index.js
+++ src/features/PersonalHomepage/Skills/Content/index.js
@@ -8,14 +8,14 @@ export const Content = ({ title, skills }) => {
 
     return (
         <>
-            <section className={`mid:mt-20 mt-8 pb-8 pt-7 px-0 mid:pl-5 ${isDark ? " bg-darkSectionBackgroundColor" : "bg-sectionBackgroundColor"}  pl-2.5`}>
+            <section className={`mid:mt-20 mt-8 pb-14 pt-7 px-0 mid:pl-5 ${isDark ? " bg-darkSectionBackgroundColor" : "bg-sectionBackgroundColor"}  pl-2.5`}>
                 <h3 className={`mid:text-3xl text-xs mid:mt-7 mt-4 font-black tracking-wider pb-3 border-b ${isDark ? "text-white" : "text-headerTextColor"} border-talesBorder`}>
                     {title}
                 </h3>
                 <ul className={`p-0 ${isDark ? " text-white" : "text-secondaryTextColor"}  grid mid:grid-cols-3 grid-cols-1 gap-y-2 gap-x-8 list-none mid:text-lg text-xs`}>
                     {skills.map(skill => (
                         <li
-                            className="before:content-['.'] h-8 mid:h-16 before:mid:pr-4 before:pr-2 before:text-blueElementColor before:align-[3px] before:text-7xl "
+                            className="before:content-['.'] h-8 mid:h-16 before:mid:pr-4 before:pr-2 before:text-blueElementColor before:mid:align-[3px] before:align-[1px] before:text-7xl "
                             key={skill.id}>
 
                             {skill.content}
