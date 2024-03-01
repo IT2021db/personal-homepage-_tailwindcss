d5473ce30147ea057de3e2f173131563fff6b2bf
Dorota Borowska <youcode2021db@gmail.com>
2024-03-01 16:56:59 +0100

    themeProvider removed, GlobalStyle has got props 'isDark'

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -1,17 +1,15 @@
 import { useSelector } from "react-redux";
-import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./GlobalStyle";
-import { lightTheme, darkTheme } from "./theme";
 import { selectIsDark } from "../../features/PersonalHomepage/ThemeSwitcher/themeSwitcherSlice";
 import { PersonalHomepage } from "../../features/PersonalHomepage";
 
 function App() {
   const isDark = useSelector(selectIsDark);
   return (
-    <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
-      <GlobalStyle />
+    <>
+      <GlobalStyle isDark={isDark} /> {/* Passing the isDark value to GlobalStyle */}
       <PersonalHomepage />
-    </ThemeProvider>
+    </>
   );
 }
 
